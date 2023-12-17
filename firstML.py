7af447d173f0df2bb68df2b1dbcd054944541869
meseret alemnew <alemnehmeseret@gmail.com>
2023-12-17 09:48:02 +0300

    Changing the Boston dataset to California housing

--- firstML.py
+++ firstML.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.datasets import fetch_california_housing
 
 # Load the Boston housing dataset
-boston = load_boston()
+boston = fetch_california_housing()
 X = boston.data
 y = boston.target
 data = pd.DataFrame(X, columns=boston.feature_names)
